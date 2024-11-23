6973bec01988213fcf55b2299bde404750983a4b
Yanting Wan <studyYanting@163.com>
2024-11-23 12:20:08 +0800

    finish 64*64

--- cachelab/cachelab-handout/trans.c
+++ cachelab/cachelab-handout/trans.c
@@ -84,34 +84,79 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
     }
     else if (M == 64 && N == 64)
     {
-        // 8*4 block, i stand for the row index of "BLOCK", j stand for the column index of "BLOCK"
-        for (i = 0; i < N; i += 4)
+        // 8*8 block, i stand for the row index of "BLOCK", j stand for the column index of "BLOCK"
+        for (i = 0; i < N; i += 8)
         {
-            for (j = 0; j < M; j += 4)
+            for (j = 0; j < M; j += 8)
             {
-                // 先读A，写入B的行， A read miss 1 and B write miss 1 per line
+                // 先读A的上半部分 (4 * 8)
                 for (i1 = 0; i1 < 4; i1++)
-                {
-                    tmp = A[i + i1][j];
-                    tmp1 = A[i + i1][j + 1];
-                    tmp2 = A[i + i1][j + 2];
-                    tmp3 = A[i + i1][j + 3];
+                {   
+                    tmp = A[i+i1][j];
+                    tmp1= A[i+i1][j+1];
+                    tmp2= A[i+i1][j+2];
+                    tmp3= A[i+i1][j+3];
+                    tmp4= A[i+i1][j+4];
+                    tmp5= A[i+i1][j+5];
+                    tmp6= A[i+i1][j+6];
+                    tmp7= A[i+i1][j+7];
 
-                    // 注意A的block(i, j) 要放到B的block(j, i)
-                    B[j + i1][i] = tmp;
-                    B[j + i1][i + 1] = tmp1;
-                    B[j + i1][i + 2] = tmp2;
-                    B[j + i1][i + 3] = tmp3;
+                    B[j][i + i1] = tmp;
+                    B[j+1][i + i1] = tmp1;
+                    B[j+2][i + i1] = tmp2;
+                    B[j+3][i + i1] = tmp3;
+                    B[j][i+i1+4] = tmp4;
+                    B[j+1][i+i1+4] = tmp5;
+                    B[j+2][i+i1+4] = tmp6;
+                    B[j+3][i+i1+4] = tmp7;
                 }
-                // 直接对B进行转置，因为B已经在cache里了，所以对B进行读写每次都是命中
-                for (i1 = 0; i1 < 4; i1++)
-                {
-                    for (j1 = i1 + 1; j1 < 4; j1++)
-                    {
-                        // 对角线不用处理
-                        tmp = B[j + i1][i + j1];
-                        B[j + i1][i + j1] = B[j + j1][i + i1];
-                        B[j + j1][i + i1] = tmp;
+
+                // 处理A的右下半部分 (4 * 4)
+                for (j1 = 0; j1 < 4; j1++){
+                    // 变量缓存A的右下部分
+                    tmp4 = A[i+4][j+j1];
+                    tmp5 = A[i+5][j+j1];
+                    tmp6 = A[i+6][j+j1];
+                    tmp7 = A[i+7][j+j1];
+
+                    // 变量缓存B的右上部分
+                    tmp = B[j+j1][i+4];
+                    tmp1 = B[j+j1][i+5];
+                    tmp2 = B[j+j1][i+6];
+                    tmp3 = B[j+j1][i+7];
+                    
+                    // 写入B的右上部分
+                    B[j+j1][i+4] = tmp4;
+                    B[j+j1][i+5] = tmp5;
+                    B[j+j1][i+6] = tmp6;
+                    B[j+j1][i+7] = tmp7;
+
+                    // 写入B的左下部分
+                    B[j+j1+4][i] = tmp;
+                    B[j+j1+4][i+1] = tmp1;
+                    B[j+j1+4][i+2] = tmp2;
+                    B[j+j1+4][i+3] = tmp3;
+                }
+
+                // 处理A的左下半部分
+                for (i1=4; i1<8;i1++){
+                    tmp = A[i+i1][j+4];
+                    tmp1 = A[i+i1][j+5];
+                    tmp2 = A[i+i1][j+6];
+                    tmp3 = A[i+i1][j+7];
+
+                    B[j+i1][i+4] = tmp;
+                    B[j+i1][i+5] = tmp1;
+                    B[j+i1][i+6] = tmp2;
+                    B[j+i1][i+7] = tmp3;
+                }
+
+                // 转置B的左下半部分
+                for (i1 = 4; i1 < 8; i1++){
+                    for (j1=i1+1; j1 < 8; j1++){
+                        tmp = B[j+i1][i+j1];
+                        B[j+i1][i+j1] = B[j+j1][i+i1];
+                        B[j+j1][i+i1] = tmp;
                     }
                 }
             }
